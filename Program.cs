f26253953b300b9ce05446ed5c3f60a8a681ae32
Dawid7212 <dawidMoeck002@gmail.com>
2025-05-16 16:12:46 +0200

    drobna zmiana

--- Program.cs
+++ Program.cs
@@ -64,7 +64,7 @@ namespace AlgorytmWsteczejPropagacjiXOR
             {
                 WylosowaneWagi[i] = (rand.NextDouble() * 2) - 1;
             }
-            WylosowaneWagi = new double[] { 0.3, 0.1, 0.2, 0.6, 0.4, 0.5, 0.9, 0.7, -0.8 };
+            //WylosowaneWagi = new double[] { 0.3, 0.1, 0.2, 0.6, 0.4, 0.5, 0.9, 0.7, -0.8 };
 
 
 
